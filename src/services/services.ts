96e2144a8af9a39708e3800c382af9becb1fb819
Maksim Malashkov <max.malaschkov@mail.ru>
2023-10-01 11:37:00 +0300

    fix: add try/catch block

--- src/services/services.ts
+++ src/services/services.ts
@@ -3,7 +3,10 @@ import { PAGE_LIMIT } from '../constants/constants';
 import { CoinAPI } from '../types/types';
 
 export const fetchCoins = async (skip: number = 0) => {
-  const { data } = await axios.get<CoinAPI>(`https://api.coinstats.app/public/v1/coins?limit=${PAGE_LIMIT}&currency=usd&skip=${skip}`);
-
-  return data.coins;
+  try {
+    const { data } = await axios.get<CoinAPI>(`https://api.coinstats.app/public/v1/coins?limit=${PAGE_LIMIT}&currency=usd&skip=${skip}`);
+    return data.coins;
+  } catch (error) {
+    console.error(error);
+  }
 }
